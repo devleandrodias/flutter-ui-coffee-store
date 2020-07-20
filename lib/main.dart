b2e91931114aa483dd919871040f0a1b759eef65
devleandrodias <leandrodbdias@gmail.com>
2020-07-20 15:17:56 -0300

    personalizando menu inferior com icones

--- lib/main.dart
+++ lib/main.dart
@@ -43,23 +43,38 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        elevation: 1,
+        selectedItemColor: Color.fromRGBO(194, 142, 121, 1),
+        showSelectedLabels: false,
         backgroundColor: Colors.white,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text(''),
+            icon: Icon(
+              Icons.home,
+              color: Color.fromRGBO(194, 142, 121, 0.3),
+            ),
+            title: Text('home'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.location_on),
-            title: Text(''),
+            icon: Icon(
+              Icons.location_on,
+              color: Color.fromRGBO(194, 142, 121, 0.3),
+            ),
+            title: Text('location'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            title: Text(''),
+            icon: Icon(
+              Icons.free_breakfast,
+              color: Color.fromRGBO(194, 142, 121, 0.3),
+            ),
+            title: Text('coffee'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            title: Text(''),
+            icon: Icon(
+              Icons.person,
+              color: Color.fromRGBO(194, 142, 121, 0.3),
+            ),
+            title: Text('user'),
           ),
         ],
       ),
